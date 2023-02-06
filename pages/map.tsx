db516e04a7e85eb0df668fcad963e36dc6592737
Takhyun Kim <kimkih1218@gmail.com>
2023-02-06 23:58:11 +0900

    feat: add map initial page

---
+++ pages/map.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Map() {
+  return (
+    <>
+      <Head>
+        <title>Map Testing</title>
+        <meta name="description" content="Map testing" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Map Page 입니다.</main>
+    </>
+  );
+}
