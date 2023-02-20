5136be61247bda30bc744eb86e0046b720b731b2
Takhyun Kim <kimkih1218@gmail.com>
2023-02-20 23:49:20 +0900

    feat: add GoggleMap and delete map page

--- components/GoogleMap/index.tsx
+++ components/GoogleMap/index.tsx
@@ -1,6 +1,10 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { useState } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 import { mapStyles } from "./googleMapStyles";
+
+// import BellIcon from "@/assets/bell.svg";
+import BellIcon from "../../assets/bell.svg";
 
 const containerStyle = {
   width: "400px",
@@ -13,6 +17,16 @@ const center = {
 };
 
 const GooglMap = () => {
+  const [markerPosition, setMarkerPosition] = useState<
+    google.maps.LatLng | google.maps.LatLngLiteral | null
+  >(null);
+
+  const handleMapClick = (event: google.maps.MapMouseEvent) => {
+    if (!event.latLng) return;
+
+    setMarkerPosition({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+  };
+
   return (
     <LoadScript
       googleMapsApiKey={
@@ -26,7 +40,16 @@ const GooglMap = () => {
         center={center}
         options={{ styles: mapStyles }}
         mapContainerStyle={containerStyle}
-      />
+        onClick={handleMapClick}
+      >
+        {markerPosition && (
+          <Marker
+            onLoad={}
+            position={markerPosition}
+            icon={{ url: require("../../assets/bell.svg") }}
+          />
+        )}
+      </GoogleMap>
     </LoadScript>
   );
 };
